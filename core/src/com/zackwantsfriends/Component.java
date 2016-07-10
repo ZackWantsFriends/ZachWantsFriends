693a188a283a632c68689d61f8556503bfb65e17
christian.greiner <christian.greiner97@gmx.de>
2016-07-10 13:43:36 +0200

    The base component class

---
+++ core/src/com/zackwantsfriends/Component.java
@@ -0,0 +1,57 @@
+package com.zackwantsfriends;
+
+/**
+ * Created by Christian on 10.07.2016.
+ */
+public abstract class Component {
+
+    private GameObject gameObject;
+
+    /**
+     * Whether or not the component is currently active.
+     *
+     * @return Returns true or false.
+     */
+    public boolean isActive() {
+        return active;
+    }
+
+    /**
+     * Toggle the activity of the component.
+     *
+     * @param active
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    private boolean active;
+
+    /**
+     * Gets the gameobject that the component is attached to.
+     *
+     * @return Returns the attached gameobject.
+     */
+    public GameObject getGameObject() {
+        return gameObject;
+    }
+
+    /**
+     * Sets the gameobject of the component.
+     *
+     * @param gameObject The gameobject.
+     */
+    public void setGameObject(GameObject gameObject) {
+        this.gameObject = gameObject;
+    }
+
+    /**
+     * renders the component.
+     *
+     * @param delta The deltatime.
+     */
+    public void render(float delta) {
+
+    }
+
+}
