34324a5c94fa4cc3feb24be6031cd4462e402bce
christian.greiner <christian.greiner97@gmx.de>
2016-07-11 18:47:15 +0200

    Added the testcomponent.

--- core/src/com/zackwantsfriends/screens/GameScreen.java
+++ core/src/com/zackwantsfriends/screens/GameScreen.java
@@ -1,12 +1,14 @@
 package com.zackwantsfriends.screens;
 
 import com.zackwantsfriends.GameObject;
+import com.zackwantsfriends.components.TestComponent;
 import com.zackwantsfriends.components.TextureComponent;
 
 public class GameScreen extends AbstractScreen {
     @Override
     public void buildStage() {
         GameObject player = new GameObject();
+        player.addComponent(new TestComponent());
         player.addComponent(new TextureComponent());
         this.addActor(player);
     }
