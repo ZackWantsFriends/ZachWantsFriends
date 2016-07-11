a3f1c8eee68969dfa66e12c37647c852771a4baf
christian.greiner <christian.greiner97@gmx.de>
2016-07-11 23:43:26 +0200

    Added MainMenuScreen

---
+++ core/src/com/zackwantsfriends/screens/MainMenuScreen.java
@@ -0,0 +1,19 @@
+package com.zackwantsfriends.screens;
+
+
+public class MainMenuScreen extends AbstractScreen {
+
+    @Override
+    public void buildStage() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+
+        getBatch().begin();
+
+        getBatch().end();
+    }
+}
