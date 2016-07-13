80cf72bbcdb2dffc3a718662591cd61bd5681c94
christian.greiner <christian.greiner97@gmx.de>
2016-07-13 21:00:03 +0200

    Added quadtree to the collisionmanager

--- core/src/com/zackwantsfriends/manager/CollisionManager.java
+++ core/src/com/zackwantsfriends/manager/CollisionManager.java
@@ -2,9 +2,18 @@ package com.zackwantsfriends.manager;
 
 import com.zackwantsfriends.components.Collision.CollisionComponent;
 import com.zackwantsfriends.gameobjects.AbstractGameObject;
+import com.zackwantsfriends.util.QuadRectangle;
+import com.zackwantsfriends.util.QuadTree;
 
 public class CollisionManager {
 
+    private QuadTree<CollisionComponent> quadTree;
+
+    public CollisionManager() {
+        quadTree = new QuadTree<CollisionComponent>(new QuadRectangle(0, 0, 0, 0), 0);
+        quadTree.maxItemByNode = 20;
+        quadTree.maxLevel = 10;
+    }
 
     public void update(AbstractGameObject gameObject) {
 
