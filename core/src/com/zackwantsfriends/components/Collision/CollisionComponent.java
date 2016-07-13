064b43807ad5a73a2d9a089246f381ea3743ff1a
christian.greiner <christian.greiner97@gmx.de>
2016-07-13 20:33:02 +0200

    Updated collision-stuff 2.0

--- core/src/com/zackwantsfriends/components/Collision/CollisionComponent.java
+++ core/src/com/zackwantsfriends/components/Collision/CollisionComponent.java
@@ -5,16 +5,27 @@ import com.badlogic.gdx.math.Vector2;
 import com.zackwantsfriends.components.AbstractComponent;
 
 public class CollisionComponent extends AbstractComponent {
-    private Rectangle AABB;
-    private Vector2 collisionDirection;
+
+    private CollisionType collisionType;
+    private Rectangle bounds;
 
     public CollisionComponent() {
-        AABB = new Rectangle(getGameObject().getX(), getGameObject().getY(),
+        bounds = new Rectangle(getGameObject().getX(), getGameObject().getY(),
                 getGameObject().getWidth(), getGameObject().getHeight());
+
+        collisionType = CollisionType.DYNAMIC;
+    }
+
+    public CollisionType getCollisionType() {
+        return collisionType;
+    }
+
+    public void setCollisionType(CollisionType collisionType) {
+        this.collisionType = collisionType;
     }
 
     @Override
     public void update(float deltaTime) {
-
+        bounds.setPosition(getGameObject().getX(), getGameObject().getY());
     }
 }
--- core/src/com/zackwantsfriends/components/Collision/CollisionData.java
+++ core/src/com/zackwantsfriends/components/Collision/CollisionData.java
@@ -2,4 +2,19 @@ package com.zackwantsfriends.components.Collision;
 
 public class CollisionData {
 
+    public CollisionComponent getOtherCollider() {
+        return otherCollider;
+    }
+
+    public CollisionSide getCollisionSide() {
+        return collisionSide;
+    }
+
+    private CollisionComponent otherCollider;
+    private CollisionSide collisionSide;
+
+    public CollisionData(CollisionComponent otherCollider, CollisionSide collisionSide) {
+        this.otherCollider = otherCollider;
+        this.collisionSide = collisionSide;
+    }
 }
