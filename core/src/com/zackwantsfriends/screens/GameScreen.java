a1edeb2e45eed9b17f892082c5c5e6e1c9af3adb
Ash Levy <ashy.of.ren@gmail.com>
2016-07-19 18:18:17 -0400

    more stuff to test with

--- core/src/com/zackwantsfriends/screens/GameScreen.java
+++ core/src/com/zackwantsfriends/screens/GameScreen.java
@@ -11,6 +11,8 @@ import com.zackwantsfriends.gameobjects.EnemyGameObject;
 import com.zackwantsfriends.gameobjects.PlayerGameObject;
 import com.zackwantsfriends.gameobjects.TileCollisionObject;
 
+import java.util.Objects;
+
 public class GameScreen extends AbstractScreen {
     private OrthogonalTiledMapRenderer tiledMapRenderer;
 
@@ -22,15 +24,15 @@ public class GameScreen extends AbstractScreen {
 
         // first layer should always be the object layer
         for (MapObject object : tiledMap.getLayers().get(0).getObjects()) {
-            if (object.getName().equals("player_spawn")) {
+            if (Objects.equals(object.getName(), "player_spawn")) {
                 float x = ((RectangleMapObject) object).getRectangle().getX();
                 float y = ((RectangleMapObject) object).getRectangle().getY();
                 addActor(new PlayerGameObject(x, y));
-            } else if (object.getName().equals("enemy_spawn")) {
+            } else if (Objects.equals(object.getName(), "enemy_spawn")) {
                 float x = ((RectangleMapObject) object).getRectangle().getX();
                 float y = ((RectangleMapObject) object).getRectangle().getY();
                 addActor(new EnemyGameObject(x, y));
-            } else if (object.getName().equals("tile_collision")) {
+            } else if (Objects.equals(object.getName(), "tile_collision")) {
                 addActor(new TileCollisionObject(((RectangleMapObject) object).getRectangle()));
             }
         }
