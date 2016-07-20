d06d2a96fdf2f0fadfddb7d5c157a6c8f7b858f0
christian.greiner <christian.greiner97@gmx.de>
2016-07-20 17:48:48 +0200

    Added ui stuff to the mainmenu

--- core/src/com/zackwantsfriends/components/TextureComponent.java
+++ core/src/com/zackwantsfriends/components/TextureComponent.java
@@ -9,22 +9,13 @@ public class TextureComponent extends AbstractComponent {
     private Texture texture;
 
     public TextureComponent(Texture texture) {
-
         this.texture = texture;
 
     }
 
     @Override
-    public void onCollision(CollisionData collisionData) {
-        super.onCollision(collisionData);
-        System.out.println("COLLIDED WITH: " + collisionData.getOtherCollider().getGameObject().getName());
-    }
-
-    @Override
     public void render(Batch batch) {
-
         batch.draw(texture, getGameObject().getX(), getGameObject().getY());
-
         super.render(batch);
     }
 }
--- core/src/com/zackwantsfriends/screens/MainMenuScreen.java
+++ core/src/com/zackwantsfriends/screens/MainMenuScreen.java
@@ -1,9 +1,18 @@
 package com.zackwantsfriends.screens;
 
-
-import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 
 public class MainMenuScreen extends AbstractScreen {
+
+    private Texture backgroundTexture;
+    private Texture logoTexture;
+
+    public MainMenuScreen() {
+        backgroundTexture = new Texture(Gdx.files.internal("backgrounds.png"));
+        logoTexture = new Texture(Gdx.files.internal("ui/logo.png"));
+
+    }
 
     @Override
     public void buildStage() {
@@ -15,6 +24,10 @@ public class MainMenuScreen extends AbstractScreen {
         super.render(delta);
 
         getBatch().begin();
+
+        getBatch().draw(backgroundTexture, 0, 0);
+
+        getBatch().draw(logoTexture, 200, 0, (int) logoTexture.getWidth() * 0.75f, (int) logoTexture.getHeight() * 0.75f);
 
         getBatch().end();
     }
